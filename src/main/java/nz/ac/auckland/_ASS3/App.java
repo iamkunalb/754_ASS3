1da74b15a1920fb9cc6152284235dd08cca3dd74
iamkunalb <kbha192@aucklanduni.ac.nz>
2019-04-08 14:57:33 +1200

    code ass3

---
+++ src/main/java/nz/ac/auckland/_ASS3/App.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland._ASS3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
